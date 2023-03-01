83e86becaf8355f97f3a367037c4dec9c3cbf4c7
pedro-l0pes <pedro.lopes@sptech.school>
2023-03-01 20:24:41 -0300

    cadastro unidade v2

--- site/src/models/unidadeModel.js
+++ site/src/models/unidadeModel.js
@@ -20,34 +20,12 @@ function entrar(email, senha) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
 function cadastrar(nome, telefone, fkEmpresa, cep, uf, cidade, logragouro, bairro, numero, complemento) {
-    cadastrarEndereco(cep, uf, cidade, logragouro, bairro, numero, complemento);
-    cadastrarUnidade(nome, telefone, fkEmpresa);
-}
-function cadastrarUnidade(nome, telefone, fkEmpresa) {
-    console.log("ACESSEI O unidade MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO unidade (nome, telefone, fkEmpresa, fkEndereco) VALUES ('${nome}', '${telefone}' 1);
-    `;
+    INSERT INTO unidade (nome, telefone, cep, sigla, cidade, logradouro, bairro, numero, complemento, fkEmpresa) VALUES ('${nome}', '${telefone}', '${cep}', '${uf}', '${cidade}', '${logragouro}', '${bairro}', '${numero}', '${complemento}' , 1);
+`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
-
-function cadastrarEndereco(cep, uf, cidade, logragouro, bairro, numero, complemento) {
-    console.log("ACESSEI O unidade MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucao = `
-        INSERT INTO endereco (cep, sigla, cidade, logradouro, bairro, numero, complemento) VALUES 
-        ('${cep}', '${uf}', '${cidade}', '${logragouro}', '${bairro}', '${numero}', '${complemento}');
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
-}
-
 
 
 function deletar(idAviso) {
