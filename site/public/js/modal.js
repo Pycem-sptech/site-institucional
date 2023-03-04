b94af90dcd7c14e36d3ba73598b9e96c9fba7973
Matheus Ribeiro <matheus.ribeiro@sptech.school>
2023-03-04 03:09:02 -0300

    Modal & Tela funcionarios

--- site/public/js/modal.js
+++ site/public/js/modal.js
@@ -33,11 +33,48 @@ function salvarEdicaoMaquina(){
     let qtdArmazenamento = document.getElementById('qtdArmazenamentoModal').value;
 
         if(unidade != undefined && unidade != '' && numeroDeSerie != undefined && numeroDeSerie != '' && processador != undefined && processador != '' && memoriaRam != undefined && memoriaRam != '' && escolherArmazenamento != undefined && escolherArmazenamento != ''&& qtdArmazenamento != undefined && qtdArmazenamento != ''){
-            alert("Dados alterados ")    
-        }else{
-            alert("Verifique os campos");
-        }
+          Swal.fire({
+            title: 'Deseja mesmo salvar as alterações?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, salvar',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Pronto!',
+                'Suas alterações foram gravadas',
+                'success'
+              )
+            }
+          })
+    }else{
+        alert("Verifique os campos");
+    }
 
+}
+
+function deletarRegistroMaquina(){
+  Swal.fire({
+      title: 'Você tem certeza?',
+      text: "Você não irá conseguir reverter isso!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, deletar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Pronto!',
+          'O registro da unidade foi deletado com sucesso!',
+          'success'
+        )
+      }
+    })
 }
 
 function salvarEdicaoUnidade(){
@@ -95,17 +132,53 @@ function deletarRegistroUnidade(){
       })
 }
 function salvarEdicaoFuncionario(){
-    let unidade = document.getElementById('escolherUnidadeModal').value;
-    let numeroDeSerie = document.getElementById('numeroDeSerieModal').value;
-    let processador = document.getElementById('processadorModal').value;
-    let memoriaRam = document.getElementById('memoriaRamModal').value;
-    let escolherArmazenamento = document.getElementById('escolherArmazenamentoModal').value;
-    let qtdArmazenamento = document.getElementById('qtdArmazenamentoModal').value;
+    let nomeFuncionario = document.getElementById('nomeFuncionarioModal').value;
+    let unidadeFuncionario = document.getElementById('unidadeFuncionarioModal').value;
+    let cargoFuncionario = document.getElementById('escolherCargoModal').value;
+    let emailFuncionario = document.getElementById('emailModal').value;
+    let cpfFuncionario = document.getElementById('cpfModal').value;
+    let senhaFuncionario = document.getElementById('senhaModal').value;
 
-        if(unidade != undefined && unidade != '' && numeroDeSerie != undefined && numeroDeSerie != '' && processador != undefined && processador != '' && memoriaRam != undefined && memoriaRam != '' && escolherArmazenamento != undefined && escolherArmazenamento != ''&& qtdArmazenamento != undefined && qtdArmazenamento != ''){
-            alert("Dados alterados ")    
-        }else{
-            alert("Verifique os campos");
-        }
+        if(nomeFuncionario != undefined && nomeFuncionario != '' && unidadeFuncionario != undefined && unidadeFuncionario != '' && cargoFuncionario != undefined && cargoFuncionario != '' && emailFuncionario != undefined && emailFuncionario != '' && cpfFuncionario != undefined && cpfFuncionario != ''&& senhaFuncionario != undefined && senhaFuncionario != ''){
+          Swal.fire({
+            title: 'Deseja mesmo salvar as alterações?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, salvar',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Pronto!',
+                'Suas alterações foram gravadas',
+                'success'
+              )
+            }
+          })
+    }else{
+        alert("Verifique os campos");
+    }
 
+}
+function deletarRegistroFuncionario(){
+  Swal.fire({
+      title: 'Você tem certeza?',
+      text: "Você não irá conseguir reverter isso!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, deletar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Pronto!',
+          'O registro do funcionário foi deletado com sucesso!',
+          'success'
+        )
+      }
+    })
 }
