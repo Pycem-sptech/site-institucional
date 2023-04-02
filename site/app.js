099a422221598fcb5f787f8c8a11cfb8679e8093
Matheus-Ribeir <matheus.ribeiro@sptech.school>
2023-04-02 00:04:09 -0300

    Mensagem do console mostrando de forma mais clara o ambiente atual

--- site/app.js
+++ site/app.js
@@ -32,9 +32,23 @@ app.use("/unidade", unidadeRouter)
 app.use("/maquina", maquinaRouter)
 
 app.listen(PORTA, function () {
-    console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
-    Você está rodando sua aplicação em Ambiente de ${process.env.AMBIENTE_PROCESSO} \n
-    \t\tSe "desenvolvimento", você está se conectando ao banco LOCAL (MySQL Workbench). \n
-    \t\tSe "producao", você está se conectando ao banco REMOTO (SQL Server em nuvem Azure) \n
-    \t\t\t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'`);
-});
+    if (process.env.AMBIENTE_PROCESSO == 'desenvolvimento') {
+        console.log(`
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n
+\t\t\t\t\tO servidor PYCEM^2 está online!
+\t\t\tAcesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n
+\t\t\t\t\t**** Ambiente de Desenvolvimento ****
+\t\t\tVocê está utilizando o banco de dados local(MySQL Workbench).\n
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n`
+        );
+    } else {
+console.log(`
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n
+|\t\t\t\t\tO servidor PYCEM^2 está online!
+|\t\t\tAcesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n
+\t\t\t\t\t**** Ambiente de Produção ****
+\t\t\tVocê está utilizando o banco de dados remoto(SQL SERVER).\n
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n`
+);}});
