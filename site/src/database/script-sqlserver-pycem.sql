7cb31ba480185df3bb3d2ae6a9eba6eed196cfee
Matheus-Ribeir <matheus.ribeiro@sptech.school>
2023-04-13 23:45:39 -0300

    Script Sql Server e Script MySql

--- site/src/database/script-sqlserver-pycem.sql
+++ site/src/database/script-sqlserver-pycem.sql
@@ -1,6 +1,7 @@
-//******************************************ATENÇÃO***********************************************
-// SCRIPT SQL SERVER ( ********NÃO UTILIZAR CREATE DATABASE OU DROP DATABASE********)
-//******************************************ATENÇÃO***********************************************
+--******************************************ATENÇÃO***********************************************
+-- SCRIPT SQL SERVER ( ********NÃO UTILIZAR CREATE DATABASE OU DROP DATABASE********)
+--******************************************ATENÇÃO***********************************************
+
 create table empresa(
 idEmpresa int primary key identity(100,1),
 nome varchar(60) not null,
@@ -10,11 +11,23 @@ email varchar(100) not null unique,
 data_cadastro datetime not null default current_timestamp
 );
 
+create table alerta(
+idAlerta int primary key identity(1,1),
+freq_alerta int not null default 5,
+cpu_alerta int not null default 50,
+cpu_critico int not null default 80,
+ram_alerta int not null default 50,
+ram_critico int not null default 80,
+hd_alerta int not null default 50,
+hd_critico int not null default 80,
+fkEmpresa int, FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
 create table usuario(
 idUsuario int identity(1,1) primary key,
 nome varchar(50) not null,
 email varchar(100) not null unique,
-cpf varchar(45) not null,
+cpf varchar(14) not null,
 senha varchar(80) not null,
 cargo varchar(10) not null, constraint chkCargo check (cargo in('Tecnico','Supervisor','Dono')),
 fkEmpresa int, FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
@@ -22,7 +35,7 @@ fkEmpresa int, FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 
 create table unidade(
 idUnidade int primary key identity(1,1),
-nome varchar(45) not null,
+nome varchar(60) not null,
 telefone varchar(15) not null unique,
 sigla CHAR(2) not null,
 cidade VARCHAR(60) not null,
@@ -30,7 +43,6 @@ logradouro VARCHAR(70) not null,
 bairro VARCHAR(70) not null, 
 numero INT not null unique,
 cep CHAR(9) not null,
-complemento VARCHAR(80),
 fkEmpresa int not null, FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
 );
 
@@ -43,11 +55,11 @@ PRIMARY KEY(fkUsuario,fkEmpresa,fkUnidade)
 
 create table totem(
 idTotem int primary key identity(1,1),
-numeroSerie varchar(45) not null,
-processador varchar(45) not null,
-ram varchar(45) not null,
-armazenamento varchar(45) not null,  constraint chkArmazenamento check (armazenamento in('HD','SSD')),
-qtdArmazenamento varchar(45) not null,
+numeroSerie varchar(30) not null,
+processador varchar(30) not null,
+ram int not null,
+tipo_armazenamento varchar(3) not null,  constraint chkArmazenamento check (tipo_armazenamento in('HD','SSD')),
+qtd_armazenamento int not null,
 estado varchar(10) not null default 'Desligado', constraint chkEstado check (estado in('Disponivel','Manutencao','Desligado')),
 fkUnidade int, FOREIGN KEY (fkUnidade) REFERENCES unidade(idUnidade) ON DELETE CASCADE
 );
@@ -57,6 +69,9 @@ idRegistro int primary key identity(1,1),
 uso_processador varchar(45),
 uso_ram varchar(45),
 uso_hd varchar(45),
+cpu_status varchar(8) not null,
+ram_status varchar(8) not null,
+hd_status varchar(8) not null,
 data_registro datetime not null default current_timestamp,
 fkTotem int, FOREIGN KEY (fkTotem) REFERENCES totem(idTotem)
 );
@@ -68,4 +83,5 @@ descricao varchar(255),
 tipo varchar(12) not null default 'Desligamento', constraint chkTipo check (tipo in('Desligamento','Sobrecarga','Outro')),
 data_relatorio date,
 fkTotem int, FOREIGN KEY (fkTotem) REFERENCES totem(idTotem)
-)
+);
+
