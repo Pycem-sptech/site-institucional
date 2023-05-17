93eac550b3668a9082fd23e81739ebf1f45140ca
pedro-l0pes <pedro.lopes@sptech.school>
2023-05-17 17:20:31 -0300

    Funções de para o crud do chamado

---
+++ site/public/js/chamado.js
@@ -0,0 +1,184 @@
+let chamadosAbertos = [];
+let chamadosFechados = [];
+let chamadosEmAndamento = [];
+let meusChamados =  
+
+
+function criarChamado(){
+    const fkMaquina = escolherMaquinaModal.value;
+    const fkUsuario = escolherAtribuicaoModal.value;
+    const fkUnidade = escolherUnidadeModal.value;
+    const dataInicio = dataInicialModal.value;
+    const dataFim = dataEncerramentoModal.value;
+    const prioridade = escolherPrioridadeModal.value;
+    const status = escolherStatusModal.value;
+    const tipo = escolherTipoModal.value;
+    const descricao = descricaoModal.value;
+    if (fkMaquina == "" || fkUsuario == "" || dataInicio == "" || dataFim == "" || prioridade == "" || status == "" || tipo == "" || fkUnidade == "" || descricao == "") {
+        toastPadrao('error', 'Preencha os campos que estão vazios!');
+        return false;
+      } else {
+        fetch("/chamado/cadastrarChamado", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+    
+            fkMaquina: fkMaquina,
+            fkUnidade: fkUnidade,
+            fkUsuario: fkUsuario,
+            dataInicio: dataInicio,
+            dataFim: dataFim,
+            prioridade: prioridade,
+            tipo: tipo,
+            status: status,
+            descricao:descricao
+          }),
+        })
+          .then(function (resposta) {
+            console.log("resposta: ", resposta);
+    
+            if (resposta.ok) {
+              toastPadrao('success', 'Cadastro realizado com sucesso!')
+            } else {
+              throw "Houve um erro ao tentar realizar o cadastro!";
+            }
+          })
+          .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+          });
+    
+        return false;
+      }
+}
+function editarChamado(idChamado){
+  const fkMaquina = escolherMaquinaModal.value;
+  const fkUsuario = escolherAtribuicaoModal.value;
+  const fkUnidade = escolherUnidadeModal.value;
+  const dataInicio = dataInicialModal.value;
+  const dataFim = dataEncerramentoModal.value;
+  const prioridade = escolherPrioridadeModal.value;
+  const status = escolherStatusModal.value;
+  const tipo = escolherTipoModal.value;
+  const descricao = descricaoModal.value;
+    if (fkMaquina == "" || fkUsuario == "" || dataInicio == "" || dataFim == "" || prioridade == "" || status == "" || tipo == "" || fkUnidade == "" || descricao == "") {
+        toastPadrao('error', 'Preencha os campos que estão vazios!');
+        return false;
+      } else {
+        fetch(`/chamado/editarChamado/${idChamado}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+    
+            fkMaquina: fkMaquina,
+            fkUnidade: fkUnidade,
+            fkUsuario: fkUsuario,
+            dataInicio: dataInicio,
+            dataFim: dataFim,
+            prioridade: prioridade,
+            tipo: tipo,
+            status: status,
+            descricao:descricao
+          }),
+        })
+          .then(function (resposta) {
+            console.log("resposta: ", resposta);
+    
+            if (resposta.ok) {
+              toastPadrao('success', 'Atualização realizada com sucesso!')
+            } else {
+              throw "Houve um erro ao tentar realizar a atualização!";
+            }
+          })
+          .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+          });
+    
+        return false;
+      }
+}
+
+
+function deletarChamado(idChamado){
+    Swal.fire({
+        title: 'Você tem certeza?',
+        text: "Você não irá conseguir reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, deletar!',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`/chamado/deletarChamado/${idChamado}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(function (resposta) {
+    
+            if (resposta.ok) {
+              Swal.fire(
+                'Pronto!',
+                'Suas alterações foram gravadas',
+                'success'
+              ).then((result) => {
+                if (result.isConfirmed) {
+                  window.location = "../chamados.html"
+                }
+              })
+            } else if (resposta.status == 404) {
+                console.log("Deu 404!");
+            } else {
+                throw ("Houve um erro ao tentar realizar a postagem! Código da resposta: " + resposta.status);
+            }
+        }).catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+        });
+        }
+      })
+}
+
+function mostrarChamado(lista){
+    for (i = 0; i < lista.length; i++) {
+        imprimirChamado(lista[i], i);
+    }
+}
+
+function imprimirChamado() {
+    
+}
+
+function filtrarChamados() {
+    const fkEmpresa = sessionStorage.FK_EMPRESA;
+    fetch(`/maquina/atualizarListaMaquinas/${fkEmpresa}`)
+        .then(function (resposta) {
+            if (resposta.ok) {
+                if (resposta.status == 204) {
+                    console.log("Nenhum resultado encontrado!!");
+                    throw "Nenhum resultado encontrado!!";
+                }
+                resposta.json().then(function (resposta) {
+
+                    for (var i = 0; i < resposta.length; i++) {
+
+                        listaMaquinas.push(resposta[i]);
+                    }
+                    console.log(listaMaquinas);
+                });
+                setTimeout(function () { mostrarTodasMaquinas(listaMaquinas) }, 300)
+            } else {
+                throw "Houve um erro na API!";
+            }
+        })
+        .catch(function (resposta) {
+            console.error(resposta);
+        });
+
+    return false;
+
+}
--- site/src/routes/chamado.js
+++ site/src/routes/chamado.js
@@ -3,19 +3,23 @@ var router = express.Router();
 
 var chamadoController = require("../controllers/chamadoController");
 
-router.get("/listar/:fkEmpresa", function (req, res) {
+router.get("/listarChamado/:fkEmpresa", function (req, res) {
     chamadoController.listar(req, res);
 });
 
-router.post("/cadastrar/:fkEmpresa", function (req, res) {
+router.get("/listarChamadoFiltrado/:fkEmpresa", function (req, res) {
+    chamadoController.listar(req, res);
+});
+
+router.post("/cadastrarChamado/:fkEmpresa", function (req, res) {
     chamadoController.cadastrar(req, res);
 })
 
-router.put("/editar/:idChamado", function (req, res) {
+router.put("/editarChamado/:idChamado", function (req, res) {
     chamadoController.editar(req, res);
 });
 
-router.delete("/deletar/:idChamado", function (req, res) {
+router.delete("/deletarChamado/:idChamado", function (req, res) {
     chamadoController.deletar(req, res);
 });
 
